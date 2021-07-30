31e421112aa85de2aaa9fbc2cf38356d3b9d9b26
Ron <sssalansss@alan>
2021-07-30 10:19:38 +0800

    name

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
+@app.route('/<name>')
+def hello(name):
+    return "Hello" + name + "!!!"
+    
 if __name__=="__main__":
     app.run()
